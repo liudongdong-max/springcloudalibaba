f304f539f680bb486fe2eb88db7dbaab27c79ae0
hnpywxxd@163.com <hnpywxxd@163.com>
2022-10-21 22:59:17 +0800

    Initial commit

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/lddong/springcloud/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package com.lddong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lddong
+ * @create 2022-10-21 21:59
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
