dfb345cfb1be03395c3589b05bb2ee6b026638ef
hnpywxxd@163.com <hnpywxxd@163.com>
2022-10-22 16:31:27 +0800

    添加sentinel --热点key，SentinelResource 注解使用

--- cloudalibaba-sentinel-service8401/src/main/java/com/lddong/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lddong/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.lddong.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,4 +22,17 @@ public class FlowLimitController {
     {
         return "------testB";
     }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "dealHandler_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "------testHotKey";
+    }
+    public String dealHandler_testHotKey(String p1, String p2, BlockException exception)
+    {
+        return "-----dealHandler_testHotKey";
+    }
+    //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
+
 }
